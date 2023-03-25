3c696dac153edd91d537bda08702efa90f1f3762
Crafterbot <118089496+CrafterBotOfficial@users.noreply.github.com>
2023-03-25 02:40:58 -0400

    Final commit

---
+++ Gravity Editor/Gravity Editor/UI/Entry.cs
@@ -0,0 +1,20 @@
+﻿using ComputerInterface.Interfaces;
+using Gravity_Editor.UI.Views;
+using System;
+using Zenject;
+
+namespace Gravity_Editor.UI
+{
+    public class Entry : IComputerModEntry
+    {
+        public string EntryName => ModInfo.ModName;
+        public Type EntryViewType => typeof(MainView);
+    }
+    public class MainInstaller : Installer
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<IComputerModEntry>().To<Entry>().AsSingle();
+        }
+    }
+}
